07058100cd6fd7cab182644ada3d7dcbba9ee508
jesusantonio30 <morales.jesusant@gmail.com>
2025-05-07 00:17:58 -0700

    WIP: Add searchResults component and partial search logic

---
+++ src/assets/data/MockSpotifyData.jsx
@@ -0,0 +1,38 @@
+const MockSpotifyData = [
+    {
+      artist: {
+        name: "2Pac",
+        id: "1ZwdS5xdxEREPySFridCfh",
+        songs: [
+          { name: "Changes", id: "3kkb6It1pDmT4wzaGZ1A2P", album: "Greatest Hits" },
+          { name: "California Love", id: "2zYzyRzz6pRmhPzyfMEC8s", album: "All Eyez On Me" },
+          { name: "Hail Mary", id: "0I3q5fE6wg7LIfHGzjv8u2", album: "The Don Killuminati: The 7 Day Theory" }
+        ]
+      }
+    },
+    {
+      artist: {
+        name: "Logic",
+        id: "4xRYI6VqpkE3UwrDrAZL8L",
+        songs: [
+          { name: "1-800-273-8255", id: "1L5tE4z0Z6Orm5tDk8wV8v", album: "Everybody" },
+          { name: "Homicide", id: "6HZILIRieu8S0iqY8kIKhj", album: "Confessions of a Dangerous Mind" },
+          { name: "Everyday", id: "3CA9pLiwRIGtUBiMjbZmRw", album: "Bobby Tarantino II" }
+        ]
+      }
+    },
+    {
+      artist: {
+        name: "Tito Double P",
+        id: "5X7UdnkYcFiDZGi3vmN7Ep",
+        songs: [
+          { name: "Andamos Mejor", id: "4ty0dDF2n9eGHYp2FnXWJ9", album: "Del Barrio Hasta Aquí" },
+          { name: "Tumbando Humo", id: "7R2S9t2WGEbrZHQ5WR5TP7", album: "Los Corridos del Humo" },
+          { name: "Siempre Ready", id: "0M56cWJx9PZMWzEpPXvRla", album: "Código 02" }
+        ]
+      }
+    }
+  ];
+  
+  export default MockSpotifyData;
+  
--- src/components/container/App.jsx
+++ src/components/container/App.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from 'react'
 import SearchBar from '../presentation/SearchBar'
+import MockSpotifyData from '../../assets/data/MockSpotifyData'
+import SearchResults from '../presentation/SearchResults'
 
 const App = () => {
 
-  // Keep track of user search
+  // Keep track of user search input
   const [searchInput, setSearchInput] = useState('');
+  // Keep track of search results
+  const [searchResults, setSearchResults] = useState([
+    {
+      name: "example track name 1",
+      artist: "example track artist 1",
+      album: "example track album 1"
+    },
+    {
+      name: "example track name 2",
+      artist: "example track artist 2",
+      album: "example track album 2"
+    }
+  ]);
 
   // Function to handle user search input
   const handleSearchChange = (e) => {
@@ -12,7 +27,15 @@ const App = () => {
   };
   // Function to handle search
   const handleSearch = () => {
-    console.log(searchInput);
+    const artistMatch = MockSpotifyData.find(({ artist }) => (
+      artist.name.toLowerCase() === searchInput.trim().toLowerCase()
+    ));
+    setSearchResults((prevResults) => (
+      {
+        ...prevResults,
+      }
+    ))
+    console.log(searchResults);
   };
 
   return (
@@ -23,6 +46,7 @@ const App = () => {
           onSearchChange={handleSearchChange}
           onSearch={handleSearch}
         />
+        <SearchResults />
       </div>
     </main>
   )
