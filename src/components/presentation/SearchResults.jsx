d3f0b043c2e0a820207560a0c9ee27e14da8dd8e
jesusantonio30 <morales.jesusant@gmail.com>
2025-05-08 15:04:45 -0700

    Merge feature/tracklist into feature/track to add Tracklist.

--- src/components/presentation/SearchResults.jsx
+++ src/components/presentation/SearchResults.jsx
@@ -1,12 +1,7 @@
 import React from 'react'
 import Tracklist from './Tracklist'
 
-<<<<<<< HEAD
-const SearchResults = ({ searchResults }) => {
-=======
 const SearchResults = ({ userSearchResults }) => {
->>>>>>> feature/tracklist
-    
   return (
     <section className='searchResults'>
        <Tracklist userSearch={userSearchResults}/>
