8bbeb402be7f46d861ec261e569f1ff7e0dbbe96
jesusantonio30 <morales.jesusant@gmail.com>
2025-05-08 23:01:22 -0700

    Updated search input type to search. To minimize merge conflicts.

--- src/components/presentation/SearchBar.jsx
+++ src/components/presentation/SearchBar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SearchBar = ({ search, onSearchChange, onSearch }) => {
   return (
     <div className='searchContainer'>
-      <input className='searchInput' type="text" name="search" id="search" placeholder='What do you want to listen to?' value={search} onChange={onSearchChange}/>
+      <input className='searchInput' type="search" name="search" id="search" placeholder='What do you want to listen to?' value={search} onChange={onSearchChange}/>
       <button className='searchButton' type="button" onClick={onSearch}>SEARCH</button>
     </div>
   )
