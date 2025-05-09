125f197fdc0ce06d2cb11747d1fdf09f55706a43
jesusantonio30 <morales.jesusant@gmail.com>
2025-05-08 22:24:53 -0700

    Updated index.html root div to main tag. Added styles to main tag in root. To Minimize merge conflicts.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const App = () => {
   return (
-    <main>
+    <>
       <h1 className='font-poppins bg-jamming-600 text-center p-3.5'>Ja<span>mmm</span>ing</h1>
       <div className="content-wrapper">
 
       </div>
-    </main>
+    </>
   )
 }
 
