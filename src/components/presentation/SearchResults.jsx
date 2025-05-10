4e466b7bcf55b13bee4fe7951ec7e6096f3a553e
jesusantonio30 <morales.jesusant@gmail.com>
2025-05-10 00:22:37 -0700

    Added styles for searchResults container. Added header for results.

--- src/components/presentation/SearchResults.jsx
+++ src/components/presentation/SearchResults.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import Tracklist from './Tracklist'
 
 const SearchResults = ({ userSearchResults }) => {
+  if(!userSearchResults.length) return null;
   return (
     <section className='searchResults'>
-       <Tracklist userSearch={userSearchResults}/>
+      <h2 className='searchResultsHeader'>Results</h2>
+      <Tracklist userSearch={userSearchResults}/>
     </section>
   )
 }
